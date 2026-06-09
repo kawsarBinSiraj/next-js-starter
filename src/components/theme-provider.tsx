397e50711d949960a8f985c173a8eaf90a7a1f29
Kawsar Bin Siraj <38612699+kawsarBinSiraj@users.noreply.github.com>
2026-06-09 15:13:07 +0600

    added gitignore file as standard

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -3,6 +3,10 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+type Props = ThemeProviderProps & {
+  children: React.ReactNode;
+};
+
+export function ThemeProvider({ children, ...props }: Props) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
